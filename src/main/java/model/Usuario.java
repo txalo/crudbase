5a2b44ede2d5dc8785562638f505844db5016aff
txalo <txalomdq@gmail.com>
2021-12-03 14:46:16 -0300

    Clase usuario

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Usuario {
+	private int ID, monedas; 
+	private String nombre;
+	private double tiempo; 
+	private boolean admin;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getMonedas() {
+		return monedas;
+	}
+	public void setMonedas(int monedas) {
+		this.monedas = monedas;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(double tiempo) {
+		this.tiempo = tiempo;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [ID=" + ID + ", nombre=" + nombre + ", monedas=" + monedas + ", tiempo=" + tiempo + ", admin="
+				+ admin + "]";
+	}
+	
+	
+	
+}
